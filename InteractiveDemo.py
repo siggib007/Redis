06937263e2e5b13d8b0052822c0aab3a5df23650
Siggi Bjarnason <siggib007@users.noreply.github.com>
2022-07-16 14:29:31 +0000

    wip

--- InteractiveDemo.py
+++ InteractiveDemo.py
@@ -15,12 +15,17 @@ import os
 import time
 import platform
 import redis
+import re
 
 # End imports
 
 # Some Globals
 lstSysArg = sys.argv
 iSysArgLen = len(lstSysArg)
+bInteractive = False
+strRedisHost = "localhost"
+iRedisPort = 6379
+iRedisDB = 0
 
 def CleanExit(strCause):
   """
@@ -79,13 +84,16 @@ def DefineMenu():
 
   dictMenu = {}
   dictMenu["help"] = "Displays this message. Can also use /h -h and --help"
-  dictMenu["init"] = "Initialize everything"
+  dictMenu["interactive"] = "Use interactive mode, where you always go back to the menu. Can also use /i and -i. Use quit to exit interactive mode"
+  dictMenu["reset"] = "Reset and initialize everything"
   dictMenu["add"]  = "Adds a new entry"
   dictMenu["list"] = "List out all entries"
 
 def ProcessCmd(strCmd):
+  global bInteractive
+  global iSysArgLen
+  global lstSysArg
 
-  print("Processing command '{}' ".format(strCmd))
   strCmd = strCmd.replace("-","")
   strCmd = strCmd.replace("/","")
   strCmd = strCmd.replace("\\","")
@@ -93,29 +101,53 @@ def ProcessCmd(strCmd):
   strCmd = strCmd.replace(">","")
   strCmd = strCmd.lower()
 
+  if strCmd[0] == "i":
+    print("Entering interactive mode. Use command exit or quit to end")
+    bInteractive = True
+    return
+
+  lstCmd = re.split(",|\s+",strCmd)
+  if len(lstCmd) > 1:
+    strCmd = lstCmd[0]
+    del lstCmd[0]
+    iCmdLen = len(lstCmd)
+  else:
+    if bInteractive or len(lstSysArg) < 3:
+      lstCmd = []
+      iCmdLen = 0
+    else:
+      lstCmd = lstSysArg
+      del lstCmd[0]
+      del lstCmd[0]
+      iCmdLen = len(lstCmd)
+  if strCmd == "q" or strCmd == "quit" or strCmd == "exit":
+    bInteractive = False
+    print("Goodbye!!!")
+    return
   if strCmd == "h":
     strCmd = "help"
   if strCmd not in dictMenu:
     print("command {} not valid".format(strCmd))
     return
+  if strCmd == "interactive":
+    bInteractive = True
   if strCmd == "help":
     DisplayHelp()
-  elif strCmd == "init":
+  elif strCmd == "reset":
     objRedis.flushdb()
     print("Redis DB has been flushed. Have a nice day")
+    bInteractive = False
   elif strCmd == "add":
-    if iSysArgLen > 2:
-      for i in range(2,iSysArgLen):
-        print("Adding {}".format(lstSysArg[i]))
-        objRedis.lpush("MyList",lstSysArg[i])
-    else:
-      strValues = input("Please provide values to be added, you can specify multiple comma seperate values: ")
-      lstValues = strValues.split(",")
-      for strValue in lstValues:
-        strValue = strValue.strip()
-        print("Adding {}".format(strValue))
-        objRedis.lpush("MyList",strValue)
+    if iCmdLen == 0:
+      strCmd = input("Please provide values to be added, you can specify multiple comma seperate values: ")
+      lstCmd = strCmd.split(",")
+    for strValue in lstCmd:
+      strValue = strValue.strip()
+      print("Adding {}".format(strValue))
+      objRedis.lpush("MyList",strValue)
   elif strCmd == "list":
+    iListLen = objRedis.llen("MyList")
+    print ("Printing out all the entries. There are {} entries.".format(iListLen))
     lstMembers = objRedis.lrange("MyList",0,-1)
     for strMember in lstMembers:
       print(strMember.decode())
@@ -137,7 +169,8 @@ def main():
   DefineMenu()
 
   ISO = time.strftime("-%Y-%m-%d-%H-%M-%S")
-  objRedis = redis.Redis(host='localhost', port=6379, db=0)
+  objRedis = redis.Redis(host=strRedisHost, port=iRedisPort, db=iRedisDB)
+
   strBaseDir = os.path.dirname(sys.argv[0])
   strRealPath = os.path.realpath(sys.argv[0])
   strRealPath = strRealPath.replace("\\","/")
@@ -150,13 +183,13 @@ def main():
   if strLogDir[-1:] != "/":
     strLogDir += "/"
 
-  if not os.path.exists (strLogDir) :
-    os.makedirs(strLogDir)
-    print("\nPath '{0}' for log files didn't exists, so I create it!\n".format(strLogDir))
+  # if not os.path.exists (strLogDir) :
+  #   os.makedirs(strLogDir)
+  #   print("\nPath '{0}' for log files didn't exists, so I create it!\n".format(strLogDir))
 
   strScriptName = os.path.basename(sys.argv[0])
-  iLoc = strScriptName.rfind(".")
-  strLogFile = strLogDir + strScriptName[:iLoc] + ISO + ".log"
+  # iLoc = strScriptName.rfind(".")
+  # strLogFile = strLogDir + strScriptName[:iLoc] + ISO + ".log"
   strVersion = "{0}.{1}.{2}".format(sys.version_info[0],sys.version_info[1],sys.version_info[2])
   strScriptHost = platform.node().upper()
 
@@ -165,7 +198,7 @@ def main():
   print("Running from: {}".format(strRealPath))
   dtNow = time.asctime()
   print("The time now is {}".format(dtNow))
-  print("Logs saved to {}".format(strLogFile))
+  # print("Logs saved to {}".format(strLogFile))
   # objLogOut = open(strLogFile,"w",1)
 
   if objRedis.exists("AppName") == 0:
@@ -185,8 +218,10 @@ def main():
     DisplayHelp()
     strCommand = input("Please provide command: ")
     ProcessCmd(strCommand)
-
-  print("Command '{}' complete".format(strCommand))
+  while bInteractive:
+    DisplayHelp()
+    strCommand = input("Please provide command: ")
+    ProcessCmd(strCommand)
 
 if __name__ == '__main__':
     main()
